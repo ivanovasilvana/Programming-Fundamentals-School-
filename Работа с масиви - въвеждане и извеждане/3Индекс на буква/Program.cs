ab5d3b14e58056c14cd7d14e0e425f2bba72e47f
ivanovasilvana <sisi_965@abv.bg>
2021-04-10 18:39:24 +0300

    3.Индекс на буква

---
+++ Работа с масиви - въвеждане и извеждане/3Индекс на буква/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _3Индекс_на_буква
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] array = new char[input.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = input[i];
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{array[i]} -> {array[i] - 97}");
+            }
+        }
+    }
+}
