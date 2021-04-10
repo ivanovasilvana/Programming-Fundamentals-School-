c4ea847f4438df1189c4fc8e0fdbc1bf57d8bd83
ivanovasilvana <sisi_965@abv.bg>
2021-04-10 19:04:39 +0300

    Update

---
+++ Работа с масиви - въвеждане и извеждане/4Преобразуване на масив в число/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+namespace _4Преобразуване_на_масив_в_число
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] newArray = new int[array.Length - 1];
+            if (array.Length == 1)
+            {
+                Console.WriteLine(array[0]);
+                return;
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < newArray.Length - i; j++)
+                {
+                    newArray[j] = array[j] + array[j + 1];
+                }
+                array = newArray;
+            }
+            Console.WriteLine(newArray[0]);
+        }
+    }
+}
---
+++ Работа с масиви - въвеждане и извеждане/5Обръщане на елементите/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _5Обръщане_на_елементите
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            int[] array = new int[length];
+            int[] newArray = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                int element = int.Parse(Console.ReadLine());
+                array[i] = element;
+            }
+            for (int i = 0; i < length; i++)
+            {
+                newArray[i] = array[(length - 1) - i];
+            }
+            Console.WriteLine(string.Join(' ', newArray));
+        }
+    }
+}
